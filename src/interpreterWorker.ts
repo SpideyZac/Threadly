ccd9c69e2a93c431869305407b0aa7dd600766b0
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2024-01-07 20:01:59 -0800

    chore: comments

--- src/interpreterWorker.ts
+++ src/interpreterWorker.ts
@@ -8,6 +8,19 @@ let workerComms: WorkerCom[] = []; // for things like signal and waitfor
 let index = 0;
 let heartbeat = false;
 
+/*
+Heart beat explained...
+When we recieve a task, we start a heartbeat. The heartbeat will run until it is told to stop.
+Certain keywords will stop the heartbeat as they require data from another thread (like waitfor).
+So, when we reach one of these keywords, we stop the heartbeat and wait for the other thread to send us data.
+Once we recieve the data, we restart the heartbeat and continue, using the new data.
+Example:
+lets say in task1 we have a waitfor keyword. We start the heartbeat and run until we reach the waitfor keyword.
+In task2 we have a signal keyword.
+While task1 waits for the signal, its heartbeat is stopped.
+When task2 sends the signal, task1's heartbeat is restarted and task1 continues.
+*/
+
 async function doHeartbeat() {
     while (heartbeat) {
         if (index >= keywords.length) {
